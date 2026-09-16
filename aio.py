cfed963042d1658ea08f47cfb9933cebc180016d
diawsz67 <sneine.pepe666@atomicmail.io>
2026-09-16 13:36:56 +0300

    Update aio.py

--- aio.py
+++ aio.py
@@ -1,15 +1,42 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-
+from telethon import events, TelegramClient
+from datetime import datetime, timedelta
 from secret import secrets
 
 
 # ==== НАСТРОЙКИ ====
 TELEGRAM_TOKEN = secrets['BOT_API_TOKEN']
+API_ID = secrets['API_ID']
+API_TOKEN = secrets['API_TOKEN']
+name_session = "first"
+targets = [-1001007302005, "@Wylsared"]
+chat_admin = 207749386
+client = TelegramClient(name_session, API_ID, API_TOKEN)
+
+
+#функция сборщик на части
+@client.on(events.NewMessage(chats = targets))
+async def all(event):
+    await client.start()
+    print("запущено успешно")
+    a = await client.get_entity(-1001007302005)
+    async for msg in client.iter_messages(a, limit=10):
+        if 'iphone' in event.text.lower(): 
+            await msg.forward_to(chat_admin)
+            print('соо получено из чата:', msg.chat.title)
+            print(msg.text)
+
+
+
+
+
+
+
 
 # Создаём экземпляры бота и диспетчера
-bot = Bot(token=TELEGRAM_TOKEN)
+'''bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher()
 
 # Обработчик команды /start
@@ -20,11 +47,12 @@ async def cmd_start(message: types.Message):
 # Обработчик любого текстового сообщения
 @dp.message()
 async def echo(message: types.Sticker):
-    await message.answer(f"Ты написал: {message.sticker.file_id}")
+    await message.answer(f"Ты написал: {message.sticker.file_id}")'''
 
 # ==== ЗАПУСК ====
 async def main():
-    await dp.start_polling(bot)
+   
+    await client.run_until_disconnected()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(all())
